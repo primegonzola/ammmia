081b23c804ae2c044de0caad57a2f0132ea18309
primegonzola <@ammmia-api-app-1620140493>
2021-05-05 12:47:08 +0200

    fixes

--- src/apps/apis/api-handler/src/server.ts
+++ src/apps/apis/api-handler/src/server.ts
@@ -1,9 +1,33 @@
 import app from './app'
+import { Sdk, TopicOptions } from "./sdk";
 
 const port = parseInt(process.env.PORT || '3000')
 
 const server = new app().Start(port)
-  .then(port => console.log(`Server running on port ${port}`))
+  .then(port => {
+    console.log(`Server running on port ${port}`);
+
+    try {
+      console.log("Creating SDK")
+      // create sdk
+      const sdk = new Sdk();
+
+      // set up to receive
+      sdk.receive(TopicOptions.Process, (message) => {
+        console.log("receiving message: " + message)
+
+        // mark as handled
+        return true;
+      });
+
+      console.log("Disposing SDK")
+      // dispose all
+      sdk.dispose();
+    }
+    catch (error) {
+      console.log("error starting sdk. " + error);
+    }
+  })
   .catch(error => {
     console.log(error)
     process.exit(1);
